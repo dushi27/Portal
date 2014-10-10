31b8899b254dc504bcad10d412309fc1651f26b2
dushi27 <dushi27@gmail.com>
2014-10-10 14:52:26 +0000

    front end updates for the application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,42 @@
-module ApplicationHelper
+module ApplicationHelper  
+  module BootstrapExtension # Override the 'text_field_tag' method defined in FormTagHelper[1]
+    FORM_CONTROL_CLASS = "form-control"
+    
+    def text_field_tag(name, value = nil, options = {})
+      class_name = options[:class]
+        if class_name.nil?
+          options[:class] = FORM_CONTROL_CLASS
+        else
+          options[:class] << " #{FORM_CONTROL_CLASS}" if
+            " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+        end
+      super
+    end
+
+  
+  def text_field(name, value = nil, options = {})
+      class_name = options[:class]
+        if class_name.nil?
+          options[:class] = FORM_CONTROL_CLASS
+        else
+          options[:class] << " #{FORM_CONTROL_CLASS}" if
+            " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+        end
+      super
+   
+  end
+  
+  def text_area(name, value = nil, options = {})
+      class_name = options[:class]
+        if class_name.nil?
+          options[:class] = FORM_CONTROL_CLASS
+        else
+          options[:class] << " #{FORM_CONTROL_CLASS}" if
+            " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+        end
+      super
+    end
+  end
+
+  include BootstrapExtension
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  root 'static_pages#home'
+
   resources :patient_profiles
 
   resources :doctor_profiles
